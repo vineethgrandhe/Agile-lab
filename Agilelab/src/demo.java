717eee969b828a8ca7e5e26c06221852c60247e6
user <user@hp>
2017-02-14 16:26:19 +0530

    Change in Vineeth's Directory

--- Agilelab/src/demo.java
+++ Agilelab/src/demo.java
@@ -3,7 +3,6 @@ public class demo {
 	public static void main(String args[])
 	{
 	System.out.println("Hello world");
-	//let me change
-	//let me change twice
+	System.out.println("Hello Vineeth !!");
 	}
 }
